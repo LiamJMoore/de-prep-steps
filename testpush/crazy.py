ea7efe114660558591cf7a622e20bfa4bc4c0f74
LiamJMoore <liamj.moore@hotmail.com>
2025-05-27 17:10:28 +0100

    like to try

---
+++ testpush/crazy.py
@@ -0,0 +1,7 @@
+my_name = "liam"
+my_age = 31
+my_country = "United Kingdom"
+
+def print_info():   
+    print(f"My name is {my_name}, I am {my_age} years old, and I live in {my_country}.")
+print_info()
